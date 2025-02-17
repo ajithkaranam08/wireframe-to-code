241c814c8e7dc43cd64c97177a51e95f451a7fcd
ajith kumar <ajithkumar@owcdomain.com>
2025-02-17 20:13:53 +0530

    .

--- app/api/ai-model/route.tsx
+++ app/api/ai-model/route.tsx
@@ -1,6 +1,7 @@
 import Constants from "@/data/Constants";
 import { NextRequest } from "next/server";
 import OpenAI from "openai"
+
 const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
     apiKey: process.env.OPENROUTER_AI_API_KEY,
--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
     domains: ['firebasestorage.googleapis.com']
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
